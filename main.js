0551f0587d0afd09ec91f52e3bf1f392e3fbff8f
JCristinaJSalas <criistiinajerez@gmail.com>
2023-09-26 13:45:19 -0500

    feat: :construction: Working on Cores section

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-import { getAllImgDespeges  } from "./storage/v3.js";
+import { getAllImgDespeges,listadoNucleos  } from "./storage/v3.js";
 
 //url que guarda las url
 const urlDespeges = "https://api.spacexdata.com/v3/launches"
+const urlNucleos = "https://api.spacexdata.com/v3/cores"
 
-console.log(await getAllImgDespeges(urlDespeges))
+//console.log(await getAllImgDespeges(urlDespeges))
+console.log(await listadoNucleos (urlNucleos))
--- storage/v3.js
+++ storage/v3.js
@@ -1,10 +1,22 @@
 // funcion que busca los despegues
 export const getAllImgDespeges = async (urlDespeges) => {
-    const respuesta = await (await fetch(urlDespeges)).json();
-    let imagenes = respuesta.map((e) => {
+    const respuestaDespeges = await (await fetch(urlDespeges)).json();
+    let imagenes = respuestaDespeges.map((e) => {
       let { mission_patch: img, mission_patch_small: img_small } = e.links;
       return { img, img_small };
     });
     return { imagenes };
   };
   
+// listado de núcleos de cohetes
+
+export const listadoNucleos = async(urlNucleos) => {
+  const respuestaNucleos = await (await fetch(urlNucleos)).json();
+  console.log(respuestaNucleos)
+  respuestaNucleos.map((i) => {
+    let {details} = i
+    
+    details === null ? "No tiene descripcion *-*-*-*-*-*-*-*-*-*" : console.log("hola" , details) 
+  })
+  
+}
