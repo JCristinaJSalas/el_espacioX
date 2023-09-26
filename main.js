a90aca10ee4cfaa53fc4945df407e43dec57b12f
JCristinaJSalas <criistiinajerez@gmail.com>
2023-09-26 13:09:08 -0500

    feat: :sparkles: Add function for the launches
    Funcion que muestra las imagenes de los cohetes

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log ('hola')
+import { getAllImgDespeges  } from "./storage/v3";
+
+//url que guarda las url
+const urlDespeges = "https://api.spacexdata.com/v3/launches"
+
+getAllImgDespeges(urlDespeges)
