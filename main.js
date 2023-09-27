f0c76627a7edff95b15249d58b8e3fbd986fc020
= <=>
2023-09-27 07:10:56 -0500

    feat: :sparkles: Create and fix the reading funtcions for the (/rockest, /cores, /ship, /capsules)
    Se crea y se modifica las funciones que leeran los datos del json

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 import { getAllImgDespeges,listadoCapsulas,listadoCohetes,listadoNavesEspaciales,listadoNucleos  } from "./storage/v3.js";
 
-//url que guarda las url
-const urlDespeges = "https://api.spacexdata.com/v3/launches"
-const urlCohetes = "https://api.spacexdata.com/v3/rockets"
-const urlNucleos = "https://api.spacexdata.com/v3/cores"
-const urlNaves = "https://api.spacexdata.com/v3/ships"
-const urlCapsulas = "https://api.spacexdata.com/v3/capsules"
+//url que guarda las url v3
+const urlDespegesV3 = "https://api.spacexdata.com/v3/launches"
+const urlCohetesV3 = "https://api.spacexdata.com/v3/rockets"
+const urlNucleosV3 = "https://api.spacexdata.com/v3/cores"
+const urlNavesV3 = "https://api.spacexdata.com/v3/ships"
+const urlCapsulasV3 = "https://api.spacexdata.com/v3/capsules"
 
-//console.log(await getAllImgDespeges(urlDespeges))
-//listadoNucleos (urlNucleos)
-//listadoNavesEspaciales(urlNaves)
-listadoChoetes(urlCohetes)
-//listadoCapsulas(urlCapsulas)
+// *************v3************/
+//getAllImgDespeges(urlDespegesV3)
+//listadoCohetes(urlCohetesV3)
+//listadoNucleos (urlNucleosV3)
+//listadoNavesEspaciales(urlNavesV3)
+//listadoCapsulas(urlCapsulasV3)
+
+
+// *************v4************/
--- storage/v3.js
+++ storage/v3.js
@@ -1,46 +1,58 @@
-// funcion que busca los despegues
+// funcion que busca los despegues  LAUNCHES
+
 export const getAllImgDespeges = async (urlDespeges) => {
-    const respuestaDespeges = await (await fetch(urlDespeges)).json();
-    let imagenes = respuestaDespeges.map((e) => {
-      let { mission_patch: img, mission_patch_small: img_small } = e.links;
-      return { img, img_small };
+  const respuestaDespeges = await (await fetch(urlDespeges)).json();
+  let imagenes = respuestaDespeges.map((e) => {
+    let { mission_patch: img, mission_patch_small: img_small } = e.links;
+    return { img, img_small };
+  });
+  imagenes.map((e) => {
+    console.log("DESPEGUES", "img2", e.img, "img2", e.img_small);
+  });
+};
+
+// listado de Cohetes   imagenes
+
+export const listadoCohetes = async (urlCohetes) => {
+  const respuestaCohetes = await (await fetch(urlCohetes)).json();
+  respuestaCohetes.map((e) => {
+    let imagenes = e.flickr_images;
+    imagenes.map((image) => {
+      console.log("Imagenes de Cohetes", image);
     });
-    return { imagenes };
-  };
-// listado de Cohetes
-export const listadoCohetes = async(urlCohetes) => {
-  const respuestaCohetes = await (await fetch (urlCohetes)).json()
-  let imagenes = respuestaCohetes.flickr_images
-  console.log(imagenes)
-}
-  
-// listado de núcleos de cohetes
+  });
+};
 
-export const listadoNucleos = async(urlNucleos) => {
+// listado de núcleos de cohetes   core_serial  details
+
+export const listadoNucleos = async (urlNucleos) => {
   const respuestaNucleos = await (await fetch(urlNucleos)).json();
-  console.log(respuestaNucleos)
+  console.log(respuestaNucleos);
   respuestaNucleos.map((i) => {
-    let {details,core_serial } = i
-    
-    details === null ? console.log(core_serial,"no details") : console.log(core_serial," *-* detalles: " , details) 
-  })
-  
-}
+    let { details, core_serial } = i;
 
-//listado de naves espaciales
+    details === null
+      ? console.log(core_serial, "--->  no details ")
+      : console.log(core_serial, " *-* detalles: ", details);
+  });
+};
 
-export const listadoNavesEspaciales = async(urlNaves) => {
-  
-  const dataNave = await (await fetch(urlNaves)).json()
+//listado de naves espaciales ship_id
+
+export const listadoNavesEspaciales = async (urlNaves) => {
+  const dataNave = await (await fetch(urlNaves)).json();
   dataNave.map((data) => {
-    data.image === null ? console.log(data.ship_id,"No tiene imagen") : console.log(data.ship_id, "IMG:" ,data.image) 
-  })
-  
-}
-//Listado de Capsulas
+    data.image === null
+      ? console.log(data.ship_id, "No tiene imagen")
+      : console.log(data.ship_id, "IMG:", data.image);
+  });
+};
+
+
+//Listado de Capsulas        capsule_serial  details
 export const listadoCapsulas = async (urlCapsulas) => {
-  const dataNave = await (await fetch(urlCapsulas)).json()
+  const dataNave = await (await fetch(urlCapsulas)).json();
   dataNave.map((capsula) => {
-    console.log(`La capsula ${capsula.capsule_serial}: ${capsula.details}`)
-  })
-}
+    console.log(`La capsula ${capsula.capsule_serial}: ${capsula.details}`);
+  });
+};
